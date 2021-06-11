a0c6153f0c2d95c8eecd19434a4646822f677a2c
VictorLuygg <42127629+VictorLuygg@users.noreply.github.com>
2021-06-11 14:52:20 -0300

    vincula usuario à leitura

--- site/script.js
+++ site/script.js
@@ -1,145 +1,148 @@
+function getUser() {
+    let user = JSON.parse(localStorage.getItem("user"));
+    return user;
+}
+
+function setUser(obj) {
+    // Recebe um objeto
+    let user = JSON.stringify(obj); // Transforma o objeto em string
+    localStorage.setItem("user", user); // Usa a string como parâmetro para setar o JSON
+}
+
 function validateUser() {
-  var login = document.getElementById("login").value;
-  var senha = document.getElementById("senha").value;
-  if ((login == "alunopuc") & (senha == "123")) {
-    window.location.href = "home.html";
-  } else {
-    alert("Login ou senha incorreto");
-  }
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")).usuarios;
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+    let user = { id: 0, nome: "" };
+    for (let i = 0; i < usuarios.length; i++) {
+        if (login == usuarios[i].login) {
+            user.id = usuarios[i].id;
+            user.nome = usuarios[i].name;
+            if (senha == usuarios[i].senha) {
+                setUser(user);
+                console.log(getUser());
+                window.location.href = "home.html";
+            } else {
+                alert("Senha incorreta!");
+            }
+            break;
+        }
+    }
+    if (user === undefined) {
+        alert("Usuario não cadastrado!");
+    }
+}
+
+function showUser() {
+    let userElement = document.createElement("p");
+    let user = getUser().nome;
+    userElement.innerHTML = "Bem vindo/a, " + user;
+    document.getElementById("home-user").appendChild(userElement);
 }
 
 function sendLink() {
-  alert(
-    "Instruções de redefinição de senha enviado para " +
-      document.getElementById("email").value
-  );
-  window.location.href = "resetPassword.html";
+    alert(
+        "Instruções de redefinição de senha enviado para " +
+            document.getElementById("email").value
+    );
+    window.location.href = "resetPassword.html";
 }
 
 function resetPassword() {
-  alert("Senha alterada com sucesso!");
-  window.location.href = "index.html";
+    alert("Senha alterada com sucesso!");
+    window.location.href = "index.html";
 }
 
 function registerUser() {
-  window.location.href = "registerUser.html";
+    window.location.href = "registerUser.html";
 }
 
 function newUser() {
-  alert("Cadastrado com sucesso!");
-  window.location.href = "index.html";
+    alert("Cadastrado com sucesso!");
+    window.location.href = "index.html";
 }
 
 function registerItem() {
-  window.location.href = "newEquipament.html";
+    window.location.href = "newEquipament.html";
 }
 
 function listarEquipamentos() {
-  let listaEquipamentos = JSON.parse(
-    localStorage.getItem("equipamentos")
-  ).equipamentos;
-  for (let i = 0; i < listaEquipamentos.length; i++) {
-    document.getElementById("listaEquipamentos").innerHTML +=
-      "<tr><td>" +
-      listaEquipamentos[i].nome +
-      "</td><td>" +
-      listaEquipamentos[i].potencia +
-      "</td></tr>";
-  }
+    let listaEquipamentos = JSON.parse(
+        localStorage.getItem("equipamentos")
+    ).equipamentos;
+    for (let i = 0; i < listaEquipamentos.length; i++) {
+        document.getElementById("listaEquipamentos").innerHTML +=
+            "<tr><td>" +
+            listaEquipamentos[i].nome +
+            "</td><td>" +
+            listaEquipamentos[i].potencia +
+            "</td></tr>";
+    }
 }
 
 function showLeitura() {
-  let leitura = JSON.parse(localStorage.getItem("leituras"));
-  console.log(leitura);
-  let tensao = 220;
-  let listaEquipamentos = JSON.parse(
-    localStorage.getItem("equipamentos")
-  ).equipamentos;
-  let potenciaAnterior = 0;
+    showUser();
 
-  for (let i = 0; i < leitura.leituras.length; i++) {
-    let horario = leitura.leituras[i].horario;
-    let corrente = leitura.leituras[i].corrente;
-    let potenciaAtual = tensao * corrente;
-    let diferencaPotencia = potenciaAtual - potenciaAnterior;
-    console.log(
-      horario,
-      corrente,
-      potenciaAtual,
-      potenciaAnterior,
-      diferencaPotencia
-    );
-    for (let j = 0; j < listaEquipamentos.length; j++) {
-      if (diferencaPotencia == listaEquipamentos[j].potencia) {
-        document.getElementById("listaLeituras").innerHTML +=
-          "<tr><td>" +
-          listaEquipamentos[j].nome +
-          " foi ligado as " +
-          horario +
-          "</td>" +
-          "<td>" +
-          potenciaAtual +
-          " kW/h </td></tr>";
-      } else {
-        if (-diferencaPotencia == listaEquipamentos[j].potencia) {
-          document.getElementById("listaLeituras").innerHTML +=
-            "<tr><td>" +
-            listaEquipamentos[j].nome +
-            " foi desligado as " +
-            horario +
-            "</td>" +
-            "<td>" +
-            potenciaAtual +
-            " kW/h </td></tr>";
+    // Primeiro definir a posição na array leituras, p/ vincular ao usuario
+    let consumidorToken;
+    var user = getUser();
+    let leituras = JSON.parse(localStorage.getItem("leituras")).leituras;
+
+    for (let i = 0; i < leituras.length; i++) {
+        if (user.id == leituras[i].consumidorId) {
+            consumidorToken = i;
+            break;
         }
-      }
     }
-    potenciaAnterior = potenciaAtual;
-  }
+
+    // Segundo listar os equipamentos referente ao usuario usando o 'consumidorToken'
+    let leitura = leituras[consumidorToken].leituras; // Lista a leitura do usuario
+    let listaEquipamentos = JSON.parse(
+        localStorage.getItem("equipamentos")
+    ).equipamentos; // Lista os equipamentos
+    let tensao = 220;
+    let potenciaAnterior = 0;
+
+    for (let i = 0; i < leitura.length; i++) {
+        let horario = leitura[i].horario;
+        let corrente = leitura[i].corrente;
+        let potenciaAtual = tensao * corrente;
+        let diferencaPotencia = potenciaAtual - potenciaAnterior;
+
+        for (let j = 0; j < listaEquipamentos.length; j++) {
+            if (diferencaPotencia == listaEquipamentos[j].potencia) {
+                document.getElementById("listaLeituras").innerHTML +=
+                    "<tr><td>" +
+                    listaEquipamentos[j].nome +
+                    " foi ligado as " +
+                    horario +
+                    "</td>" +
+                    "<td>" +
+                    potenciaAtual +
+                    " W/h </td></tr>";
+            } else {
+                if (-diferencaPotencia == listaEquipamentos[j].potencia) {
+                    document.getElementById("listaLeituras").innerHTML +=
+                        "<tr><td>" +
+                        listaEquipamentos[j].nome +
+                        " foi desligado as " +
+                        horario +
+                        "</td>" +
+                        "<td>" +
+                        potenciaAtual +
+                        " W/h </td></tr>";
+                }
+            }
+        }
+        potenciaAnterior = potenciaAtual;
+    }
 }
 
 function newItem() {
-  alert("Cadastrado com sucesso!");
-  window.location.href = "equipaments.html";
+    alert("Cadastrado com sucesso!");
+    window.location.href = "equipaments.html";
 }
 
 function teste() {
-  console.log("Tudo Certo");
-}
-
-function processo() {
-  var localStorage = window.localStorage;
-  var leitura = localStorage.getItem("123456");
-  var tensao = 220;
-  var objLeitura = JSON.parse(leitura);
-  var jsonEquipamentos = localStorage.getItem("equipamentos");
-  var objEquipamentos = JSON.parse(jsonEquipamentos);
-  alert("Quantidade de equipamentos: " + objEquipamentos.equipamentos.length);
-  var potenciaAnterior = 0;
-  for (let i = 0; i < objLeitura.leituras.length; i++) {
-    let horario = objLeitura.leituras[i].horario;
-    let corrente = objLeitura.leituras[i].corrente;
-    let potenciaAtual = tensao * corrente;
-    let diferencaPotencia = potenciaAtual - potenciaAnterior;
-    for (let j = 0; j < objEquipamentos.equipamentos.length; j++) {
-      if (diferencaPotencia == objEquipamentos.equipamentos[j].potencia) {
-        alert(
-          "O equipamento " +
-            objEquipamentos.equipamentos[j].nome +
-            " foi ligado as " +
-            horario
-        );
-      } else {
-        if (-diferencaPotencia == objEquipamentos.equipamentos[j].potencia) {
-          alert(
-            "O equipamento " +
-              objEquipamentos.equipamentos[j].nome +
-              " foi desligado as " +
-              horario
-          );
-        }
-      }
-    }
-    potenciaAnterior = potenciaAtual;
-  }
+    console.log("Tudo Certo");
 }
