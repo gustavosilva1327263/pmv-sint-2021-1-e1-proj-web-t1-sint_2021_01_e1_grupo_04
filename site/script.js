97a0ff357ef4f6c95f58a715238f9ea1e66083cc
gustavosilva1327263 <81271938+gustavosilva1327263@users.noreply.github.com>
2021-06-25 17:34:28 -0300

    site atualizado

--- site/script.js
+++ site/script.js
@@ -5,6 +5,7 @@ let equipamentosStr = ls.getItem("equipamentos");
 //converte Json em objeto JavaScript 
 let listaEquipamentos = JSON.parse(equipamentosStr);
 let tensao = 220;
+var index = 0;
 
 function getUser() {
     let user = JSON.parse(localStorage.getItem("user"));
@@ -22,8 +23,8 @@ function validateUser() {
     let senha = document.getElementById("password").value;
     let user = { id: undefined, nome: "" };
     for (let i = 0; i < usuarios.length; i++) {
-        if(login=="admin" && senha=="admin"){
-            window.location.href="newItem.html";
+        if (login == "admin" && senha == "admin") {
+            window.location.href = "newItem.html";
         }
         if (login == usuarios[i].login) {
             user.id = usuarios[i].id;
@@ -38,7 +39,7 @@ function validateUser() {
             break;
         }
     }
-    if (user.id === undefined) {
+    if (user.id === undefined && login !== "admin") {
         alert("Usuario não cadastrado!");
     }
 }
@@ -65,7 +66,7 @@ function registerNewUser() {
     var password = document.getElementById('password').value;
 
     if (login != "" && password != "" && nome != "" && email != "" && telefone != "") {//checa se o usuario preencheu os dados de login e senha
-       
+
         var index = parseInt(listaUsuarios.usuarios.length);// transforma o numero do index do array em uma variavel do tipo inteiro
         var id = index++;//soma o index + 1 para obter o ID
         alert("Usuario cadastrado com sucesso");// envia uma alerta de cadastro concluido
@@ -93,7 +94,7 @@ function registerNewItem() {
         listaEquipamentos['equipamentos'].push({ "nome": inputnome, "potencia": inputpotencia });
         window.location.href = "itemsList.html";
 
-    // Alerta caso o usuario não tenha preenchido dados de cadastro 
+        // Alerta caso o usuario não tenha preenchido dados de cadastro 
     } else {
         alert("Preencha todos os dados")
     }
@@ -135,13 +136,93 @@ function showUser() {
     document.getElementById("home-user").appendChild(userElement);
 }
 
-function showLeitura() {
-   showUser();
 
+// function showLeitura() {
+//     showUser();
+
+//     // Primeiro definir a posição na array leituras, p/ vincular ao usuario
+//     let consumidorToken;//
+//     var user = getUser();
+//     let leituras = JSON.parse(localStorage.getItem("leituras")).leituras;
+
+//     for (let i = 0; i < leituras.length; i++) {
+//         if (user.id == leituras[i].consumidorId) {
+//             consumidorToken = i;
+//             break;
+//         }
+//     }
+
+//     // Segundo listar os equipamentos referente ao usuario usando o 'consumidorToken'
+//     let leitura = leituras[consumidorToken].leituras; // Lista a leitura do usuario
+//     let listaEquipamentos = JSON.parse(
+//         localStorage.getItem("equipamentos")
+//     ).equipamentos; // Lista os equipamentos
+//     let tensao = 220;
+//     let potenciaAnterior = 0;
+
+//     for (let i = 0; i < leitura.length; i++) {//Percorre o vetor de chaves e para cada chave encontrada, executa o código abaixo
+//         let horario = leitura[i].horario;
+//         let corrente = leitura[i].corrente;
+//         let potenciaAtual = tensao * corrente;
+//         let diferencaPotencia = potenciaAtual - potenciaAnterior;
+//         let linha = document.createElement("tr"); //Cria um novo elemento TR (linha)
+//         let coluna = document.createElement("td"); //Cria um novo elemento TD (celula de dados) 
+//         console.log(horario + "   " + diferencaPotencia + " pa:" + potenciaAtual + "potant" + potenciaAnterior);
+
+//         for (let j = 0; j < listaEquipamentos.length; j++) {
+//             // console.log(index);
+//             if (diferencaPotencia == listaEquipamentos[j].potencia) {
+//                 equipamento = listaEquipamentos[j].nome;
+
+//                 coluna = document.createElement("td");
+//                 coluna.innerHTML += listaEquipamentos[j].nome + " foi ligado as " + horario;
+//                 // console.log(listaEquipamentos[index].nome + " foi ligado as " + horario)
+//                 linha.appendChild(coluna);
+//                 coluna = document.createElement("td");
+//                 coluna.innerHTML += potenciaAtual + " W/h"
+//                 linha.appendChild(coluna);
+//             } else if (-diferencaPotencia == listaEquipamentos[j].potencia) {
+//                 coluna = document.createElement("td"); //Cria um novo elemento TD
+//                 coluna.innerHTML += listaEquipamentos[j].nome + " foi desligado as " + horario;
+//                 linha.appendChild(coluna);
+//                 coluna = document.createElement("td");
+//                 coluna.innerHTML += potenciaAtual + " W/h";
+//                 linha.appendChild(coluna);
+
+//             }
+
+//             potenciaAnterior = potenciaAtual;
+//         }
+
+//         console.log(potenciaAtual);
+//         console.log(potenciaAnterior);
+//         document.getElementById("listaConsumo").appendChild(linha);
+//     }
+// }
+
+function goHome() {
+    window.location.href = "index.html";
+}
+var i = -1;
+var potenciaAnterior = 0;
+
+function clearTable() {
+    if (document.getElementById("listaConsumo").rows.length > 1) {
+        document.getElementById("listaConsumo").deleteRow(1);
+    }
+}
+function proximaLeitura() {
+    // clearTable();
+    //showUser();
+    i++;
     // Primeiro definir a posição na array leituras, p/ vincular ao usuario
-    let consumidorToken;
+    let consumidorToken;//
     var user = getUser();
     let leituras = JSON.parse(localStorage.getItem("leituras")).leituras;
+    let linha = document.createElement("tr"); //Cria um novo elemento TR (linha)
+    let coluna = document.createElement("td");
+
+
 
     for (let i = 0; i < leituras.length; i++) {
         if (user.id == leituras[i].consumidorId) {
@@ -149,50 +230,55 @@ function showLeitura() {
             break;
         }
     }
-    
-    // Segundo listar os equipamentos referente ao usuario usando o 'consumidorToken'
+
+    // Segundo listar os equipamentos referente ao usuario usandhorao o 'consumidorToken'
     let leitura = leituras[consumidorToken].leituras; // Lista a leitura do usuario
     let listaEquipamentos = JSON.parse(
         localStorage.getItem("equipamentos")
     ).equipamentos; // Lista os equipamentos
     let tensao = 220;
-    let potenciaAnterior = 0;
 
-    for (let i = 0; i < leitura.length; i++) {//Percorre o vetor de chaves e para cada chave encontrada, executa o código abaixo
-                let horario = leitura[i].horario;
-                let corrente = leitura[i].corrente;
-                let potenciaAtual = tensao * corrente;
-                let diferencaPotencia = potenciaAtual - potenciaAnterior;
-                let linha = document.createElement("tr"); //Cria um novo elemento TR (linha)
-                let coluna = document.createElement("td"); //Cria um novo elemento TD (celula de dados) 
-       
-                for (let j = 0; j < listaEquipamentos.length; j++) {
-                    console.log(diferencaPotencia);
-                    if (diferencaPotencia == listaEquipamentos[j].potencia) {
-                        equipamento = listaEquipamentos[j].nome;
-                        coluna = document.createElement("td");
-                        coluna.innerHTML += listaEquipamentos[j].nome + " foi ligado as " + horario;
-                        linha.appendChild(coluna);
-                        coluna = document.createElement("td");
-                        coluna.innerHTML +=  potenciaAtual + " W/h"
-                        linha.appendChild(coluna);
-                    } else if (-diferencaPotencia == listaEquipamentos[j].potencia) {
-                        coluna = document.createElement("td"); //Cria um novo elemento TD
-                        coluna.innerHTML += listaEquipamentos[j].nome + " foi desligado as " + horario;
-                        linha.appendChild(coluna);
-                        coluna = document.createElement("td");
-                        coluna.innerHTML +=   potenciaAtual +" W/h";
-                        linha.appendChild(coluna);
-        
-                    }
-        
-                    potenciaAnterior = potenciaAtual;
-                }
-                document.getElementById("listaConsumo").appendChild(linha);
-            }
+
+    if (i == leitura.length) {
+        alert("Todo o histórico ja foi exibido");
+        return;
+    }
+    //Percorre o vetor de chaves e para cada chave encontrada, executa o código abaixo
+
+    let horario = leitura[i].horario;
+    let corrente = leitura[i].corrente;
+    let potenciaAtual = tensao * corrente;
+    let diferencaPotencia = potenciaAtual - potenciaAnterior;
+
+    for (let j = 0; j < listaEquipamentos.length; j++) {
+
+        if (diferencaPotencia == listaEquipamentos[j].potencia) {
+            equipamento = listaEquipamentos[j].nome;
+
+            coluna = document.createElement("td");
+            coluna.innerHTML = listaEquipamentos[j].nome + " foi ligado as " + horario;
+            // console.log(listaEquipamentos[index].nome + " foi ligado as " + horario)
+            linha.appendChild(coluna);
+            coluna = document.createElement("td");
+            coluna.innerHTML = potenciaAtual + " W/h"
+            linha.appendChild(coluna);
+        } else if (-diferencaPotencia == listaEquipamentos[j].potencia) {
+            coluna = document.createElement("td"); //Cria um novo elemento TD
+            coluna.innerHTML = listaEquipamentos[j].nome + " foi desligado as " + horario;
+            linha.appendChild(coluna);
+            coluna = document.createElement("td");
+            coluna.innerHTML = potenciaAtual + " W/h";
+            linha.appendChild(coluna);
+
+        }
+
+        potenciaAnterior = potenciaAtual;
+
+    }
+
+    console.log("potencia atual : " + potenciaAtual);
+    console.log(" potencia anterior" + potenciaAnterior);
+    document.getElementById("listaConsumo").appendChild(linha);
+
+
 }
-
-function goHome(){
-    window.location.href="index.html";
-}
-
